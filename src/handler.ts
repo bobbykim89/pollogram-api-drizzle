e8808b3148f8ef2f77f4d4857b94451ed5e14ddf
Sihun Kim <skim585@asu.edu>
2025-05-09 13:40:20 -0700

    feat(*): update build script
    update build script to use different one

--- src/handler.ts
+++ src/handler.ts
@@ -4,12 +4,12 @@ import { bootstrap } from './index'
 
 const app = bootstrap()
 
-// const handler = handle(app)
+const handler = handle(app)
 
-export default handle(app)
-// export const GET = handler
-// export const POST = handler
-// export const PATCH = handler
-// export const PUT = handler
-// export const DELETE = handler
-// export const OPTIONS = handler
+// export default handle(app)
+export const GET = handler
+export const POST = handler
+export const PATCH = handler
+export const PUT = handler
+export const DELETE = handler
+export const OPTIONS = handler
