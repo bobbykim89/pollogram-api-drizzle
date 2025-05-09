d259855cec0b73ff7038f17e3157453b6c263825
Sihun Kim <skim585@asu.edu>
2025-05-09 13:59:21 -0700

    feat(*): add index.html file for placeholder
    add index.html file as a placeholder

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 import { Hono } from 'hono'
 import { prettyJSON } from 'hono/pretty-json'
-import { appendTrailingSlash } from 'hono/trailing-slash'
 
 export const bootstrap = () => {
   const app = new Hono()
   app
     .basePath('/api')
     .use(prettyJSON())
-    .use(appendTrailingSlash())
     .get('/', (c) => {
       return c.json(
         {
           message: 'Hello from pollito!',
+          name: 'Manguito Lovebird',
         },
         200
       )
