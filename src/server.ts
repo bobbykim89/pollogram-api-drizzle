bb0d05eb381186f91e061739085ffdb780cba68f
Sihun Kim <skim585@asu.edu>
2025-05-09 11:06:44 -0700

    feat(*): update setup
    update build setup

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import { serve } from "@hono/node-server";
-import { bootstrap } from "./index";
+import { serve } from '@hono/node-server'
+import { bootstrap } from './index'
 
-const NODE_ENV = process.env.NODE_ENV;
+const NODE_ENV = process.env.NODE_ENV
 
-if (NODE_ENV !== "production") {
+if (NODE_ENV !== 'production') {
   serve(
     {
       fetch: bootstrap().fetch,
       port: 8000,
     },
     (info) => console.info(`Listening on http://localhost:${info.port}`)
-  );
+  )
 }
